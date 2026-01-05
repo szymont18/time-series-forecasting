172eba395bc96ad14a04a6db256fb61e361ea3ac
szymont18 <szymont181@gmail.com>
2026-01-05 20:36:10 +0100

    Add Tranformer models: TFT, AUTOFORMER, ITRANSFORMER

--- src/scripts/models/forecaster_utils.py
+++ src/scripts/models/forecaster_utils.py
@@ -1,20 +1,36 @@
 import importlib
 from enum import Enum
 from typing import Type, Dict, Any
+from neuralforecast import NeuralForecast
 from sktime.forecasting.base import BaseForecaster
+import pandas as pd
+from sktime.split import temporal_train_test_split
 
 class ModelName(Enum):
+    # Without Transformers
     DLINEAR = "DLinear"
     CATS = "CATS"
-    PATCHTST = "PATCHTST"
     CHRONOS = "CHRONOS"
+    AUTOARIMA = "AUTOARIMA"
 
+    # With Transformers
+    PATCHTST = "PATCHTST"
+    ITRANSFORMER = "ITRANSFORMER"
+    AUTOFORMER = "AUTOFORMER"
+    TFT = "TEMPORALFUSIONTRANSFORMER"
+
+
+NEURAL_FORECAST_MODELS = [ModelName.TFT, ModelName.AUTOFORMER, ModelName.ITRANSFORMER]
 
 MODEL_IMPORT_MAP = {
     ModelName.DLINEAR: ("sktime.forecasting.ltsf", "LTSFDLinearForecaster"),
     ModelName.CATS: ("src.scripts.models.ltsf_cats_forecaster", "LTSFCatsForecaster"),
     ModelName.PATCHTST: ("sktime.forecasting.patch_tst", "PatchTSTForecaster"),
-    ModelName.CHRONOS: ("sktime.forecasting.chronos", "ChronosForecaster")
+    ModelName.CHRONOS: ("sktime.forecasting.chronos", "ChronosForecaster"),
+    ModelName.ITRANSFORMER: ("neuralforecast.models", "iTransformer"),
+    ModelName.AUTOFORMER: ("neuralforecast.models", "Autoformer"),
+    ModelName.TFT: ("neuralforecast.models", "TFT"),
+    ModelName.AUTOARIMA: ("sktime.forecasting.statsforecast", "StatsForecastAutoARIMA")
 }
 
 DEFAULT_PARAMS = {
@@ -57,7 +73,6 @@ DEFAULT_PARAMS = {
             "per_device_train_batch_size": 16,
         }
     },
-
     ModelName.CHRONOS: {
         "model_path": "amazon/chronos-t5-small",
         "config": {
@@ -66,8 +81,79 @@ DEFAULT_PARAMS = {
             "top_k": 100,
             "top_p": 0.2
         }
+    },
+
+    ModelName.ITRANSFORMER: {
+        "h": 200,
+        "n_series": 1,
+        "input_size": 96,
+        "hidden_size": 128,
+        "n_heads": 2,
+        "e_layers": 2,
+        "d_layers": 1,
+        "d_ff": 4,
+        "factor": 1,
+    },
+
+    ModelName.AUTOFORMER: {
+        "h": 200,
+        "input_size": 24,
+        "hidden_size": 16,
+        "conv_hidden_size": 32,
+        "n_head": 2,
+        "max_steps": 300
+    },
+
+    ModelName.TFT: {
+        "h": 200,
+        "input_size": 48,
+        "hidden_size": 20,
+        "grn_activation": "ELU",
+        "rnn_type": "lstm",
+        "n_rnn_layers": 1,
+        "max_steps": 300
+    },
+
+    ModelName.AUTOARIMA: {
+        "seasonal": True
     }
 }
+
+def wide_to_long_df(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.reset_index(drop=True).to_frame(name="y")
+
+    df["ds"] = pd.date_range(
+        start="2024-01-01",
+        periods=len(df),
+        freq="D"
+    )
+
+    df["unique_id"] = "series_1"
+
+    return df[["unique_id", "ds", "y"]]
+def get_train_test(model_name: ModelName, raw_data: pd.DataFrame):
+    train_data, test_data = temporal_train_test_split(raw_data, test_size=0.2)
+
+    if model_name in NEURAL_FORECAST_MODELS:
+        train_data, test_data = wide_to_long_df(train_data), wide_to_long_df(test_data)
+
+    return train_data, test_data
+
+def fit_predict(model, model_name: ModelName, train_data: pd.DataFrame, test_data: pd.DataFrame):
+    if model_name in NEURAL_FORECAST_MODELS:
+        model.fit(df=train_data)
+        y_pred = model.predict(test_data)
+        y_pred = pd.Series(
+            y_pred.iloc[:, -1].values,
+            index=range(len(train_data), len(train_data) + len(y_pred))
+        )
+        return y_pred
+
+    fh = list(range(1, len(test_data) + 1))
+    model.fit(y=train_data, fh=fh)
+
+    y_pred = model.predict(fh=fh)
+    return y_pred
 
 def get_forecaster_class(model_name: ModelName) -> Type[BaseForecaster]:
     if model_name not in MODEL_IMPORT_MAP:
@@ -84,12 +170,15 @@ def get_forecaster_class(model_name: ModelName) -> Type[BaseForecaster]:
         raise ImportError(f"Błąd ładowania klasy {class_name} z modułu {module_name}. Sprawdź pliki: {e}")
 
 
-def create_model_instance(model_name: ModelName, config_params: Dict[str, Any] = None) -> BaseForecaster:
+def create_evaluation(model_name: ModelName, raw_data: pd.DataFrame, config_params: Dict[str, Any] = None):
     ForecasterClass = get_forecaster_class(model_name)
 
     if config_params is None:
         config_params = DEFAULT_PARAMS[model_name]
 
     forecaster_instance = ForecasterClass(**config_params)
+    if model_name in NEURAL_FORECAST_MODELS:
+        forecaster_instance = NeuralForecast(models=[forecaster_instance], freq='D')
+    train_data, test_data = get_train_test(model_name, raw_data)
 
-    return forecaster_instance
+    return forecaster_instance, train_data, test_data
--- src/scripts/run_train_evaluate.py
+++ src/scripts/run_train_evaluate.py
@@ -1,28 +1,32 @@
+import os
+
+import pandas as pd
+
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
+
 from matplotlib import pyplot as plt
 from sktime.performance_metrics.forecasting import MeanAbsoluteError, MeanAbsoluteScaledError
 from sktime.utils import plot_series
 
 from src.scripts.forecasting_utils import load_data_from_file
-from src.scripts.models.forecaster_utils import create_model_instance, ModelName
-from sktime.split import temporal_train_test_split
+from src.scripts.models.forecaster_utils import create_evaluation, ModelName, fit_predict
 
 
 if __name__ == '__main__':
     file_path = "TSB/processed/TSB-U/149_Stock_id_1_Finance_tr_500_1st_7.csv"
     raw_data = load_data_from_file(file_path)[:1000]
+    model_type = ModelName.TFT
 
-    train_data, test_data = temporal_train_test_split(raw_data, test_size=0.2)
-    fh = list(range(1, len(test_data) + 1))
+    model, train_data, test_data = create_evaluation(model_name=model_type, raw_data=raw_data)
+    y_pred = fit_predict(model, model_type, train_data, test_data)
 
-    cats_model = create_model_instance(model_name=ModelName.CHRONOS)
-
-    cats_model.fit(y=train_data, fh=fh)
-
-    y_pred = cats_model.predict(fh=fh)
+    if not isinstance(train_data, pd.Series):
+        train_data = train_data["y"]
+        test_data = test_data["y"]
 
     mae = MeanAbsoluteError()(test_data, y_pred)
-    mase = MeanAbsoluteScaledError()(test_data, y_pred, y_train=train_data)
     print(f"MAE: {mae:.4f}")
+    mase = MeanAbsoluteScaledError()(test_data, y_pred, y_train=train_data)
     print(f"MASE: {mase:.4f}")
 
     plot_series(raw_data, y_pred, labels=["True", "Predicted"])
